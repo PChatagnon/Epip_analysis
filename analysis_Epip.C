7e8c7a1130868395fb595e9994b380ae58e9ce76
Pierre Chatagnon <pierrec@jlab.org>
2023-10-05 14:21:11 -0400

    add angle corrected

--- analysis_Epip.C
+++ analysis_Epip.C
@@ -468,7 +468,7 @@ int analysis_Epip()
 				outVars["PCAL_W_elec"] = ev.Electron.W_CALO(PCAL);
 				outVars["PCAL_energy_elec"] = ev.Electron.Energy(ECAL, PCAL);
 				outVars["ECIN_energy_elec"] = ev.Electron.Energy(ECAL, ECIN);
-				outVars["angle_e_pion"] = (Pion->Angle((Electron)->Vect()));
+				outVars["angle_e_pion"] = (ev.Pion.Vector.Angle(ev.Electron.Vector.Vect()));
 
 				outVars["Q2_Gen"] = MC_ev.Q2_Gen;
 				outVars["W_Gen"] = MC_ev.W_Gen;
